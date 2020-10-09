58f562bbf131c7c553effc03b9554e2c6fc36c92
Mridul Awasthi <mridulawasthi@Mriduls-MacBook-Pro.local>
2020-10-09 12:17:49 -0400

    completed functionality and aesthetics

--- conway.js
+++ conway.js
@@ -1,43 +1,78 @@
 // Mridul Awasthi
 // CS 375-001
+// Homework 3
 
-function createBoard(row, col) {
+// Note: Every white cell is dead. Every non-white cell is alive.
+// Note: Have also done the extra credit part.
 
-	var count = 0;
-	let container = document.getElementById("table-container");
-	let table = document.createElement("table");
+var interv = 0;
 
-	for (let i = 0; i < row; i++) {
+function startInterval() {
+	clearInterval(interv);	
+	interv = setInterval(stepBoard, 100);
+}
+
+function stopInterval() {	
+	clearInterval(interv);
+}
+
+function boardReset(row, col) {
+
+    var table = document.getElementById("board");
+    var countRows = table.rows.length;
+    for (var i = 0; i < countRows; i++) {
+        table.deleteRow(0);
+    }
+
+	for (var i = 0; i < row; i++) {
         var rows = table.insertRow(i);
         if(i%2 == 0){
             var color = 'black';
         } else {
             var color = 'white';
         }
-		console.log('check');
-		for (let j = 0; j < col; j++) {
+
+		for (var j = 0; j < col; j++) {
             var cols = rows.insertCell(j);
             cols.style.backgroundColor = color;
+
             if(color === 'black') {
                 color = 'white';
             } else {
                 color = 'black';
             }
-			count += 1;
 		}
 	}
-	container.append(table);
 }
-createBoard(25, 25);
 
-function stepBoard(board) {
+function randomBoard(row, col) {
 
-    var h = board.length;
-    if(!h) {
-        return board;
+    var table = document.getElementById("board");
+    var countRows = table.rows.length;
+    for (var i = 0; i < countRows; i++) {
+        table.deleteRow(0);
     }
-    var w = board[0].length;
 
+    var colorArr = ['gray', 'red', 'blue', 'yellow', 'orange', 'purple', 'green', 'pink', 'black', 'brown'];
+	for (var i = 0; i < row; i++) {
+        var rows = table.insertRow(i);
+        for (var j = 0; j < col; j++) {
+            var cols = rows.insertCell(j);
+            if((i+j)%Math.floor(Math.random()*10) == 0){
+                cols.style.backgroundColor = colorArr[Math.floor(Math.random() * 10)];
+            } else {
+                cols.style.backgroundColor = 'white'; // to avoid way too low number of dead cells
+            }
+            
+        }
+	}
+}
+
+function stepBoard() {
+    var colorArr = ['gray', 'red', 'blue', 'yellow', 'orange', 'purple', 'green', 'pink', 'black', 'brown'];
+    var board = document.getElementById('board');
+    var h = board.rows.length;
+    var w = board.rows[0].cells.length;
     // initialize duplicate array
 
     var newBoard = [];
@@ -51,10 +86,7 @@ function stepBoard(board) {
 
     for(var i = 0; i < h; i++ ) {
         for(var j = 0; j < w; j++ ) {
-            if(board[i][j] === '') {
-                return board;
-            }
-
+            
             var t = 0;
             var b = 0;
             var l = 0;
@@ -68,7 +100,7 @@ function stepBoard(board) {
                 
             if(i - 1 === -1 ) {
                 t = 0;
-            } else if(board[i-1][j] ) {
+            } else if(board.rows[i-1].cells[j].style.backgroundColor !== 'white') {
                 t = 1;
             } else {
                 t = 0;
@@ -78,7 +110,7 @@ function stepBoard(board) {
 
             if(i + 1 === h) {
                 b = 0;
-            } else if(board[i+1][j] ) {
+            } else if(board.rows[i+1].cells[j].style.backgroundColor !== 'white') {
                 b = 1;
             } else {
                 b = 0;
@@ -88,7 +120,7 @@ function stepBoard(board) {
 
             if(j - 1 === -1) {
                 l = 0;
-            } else if(board[i][j-1]) {
+            } else if(board.rows[i].cells[j-1].style.backgroundColor !== 'white') {
                 l = 1;
             } else {
                 l = 0;                
@@ -98,7 +130,7 @@ function stepBoard(board) {
 
             if(j + 1 === w) {
                 r = 0;
-            } else if(board[i][j + 1] ) {
+            } else if(board.rows[i].cells[j+1].style.backgroundColor !== 'white' ) {
                 r = 1;
             } else {
                 r = 0;
@@ -108,7 +140,7 @@ function stepBoard(board) {
 
             if(i - 1 === -1 || j - 1 === -1) {
                 tl = 0;
-            } else if(board[i - 1][j - 1] ) {
+            } else if(board.rows[i-1].cells[j-1].style.backgroundColor !== 'white') {
                 tl = 1;
             } else {
                 tl = 0;
@@ -118,7 +150,7 @@ function stepBoard(board) {
 
             if(i - 1 === -1 || j + 1 === w) {
                 tr = 0;
-            } else if(board[i - 1][j + 1] ) {
+            } else if(board.rows[i-1].cells[j+1].style.backgroundColor !== 'white') {
                 tr = 1;
             } else {
                 tr = 0;
@@ -128,7 +160,7 @@ function stepBoard(board) {
 
             if(i + 1 === h || j - 1 === -1) {
                 bl = 0;
-            } else if(board[i + 1][j - 1] ) {
+            } else if(board.rows[i+1].cells[j-1].style.backgroundColor !== 'white') {
                 bl = 1;
             } else {
                 bl = 0;
@@ -138,7 +170,7 @@ function stepBoard(board) {
 
             if(i + 1 === h || j + 1 === w) {
                 br = 0;
-            } else if(board[i + 1][j + 1] ) {
+            } else if(board.rows[i+1].cells[j+1].style.backgroundColor !== 'white') {
                 br = 1;
             } else {
                 br = 0;
@@ -147,7 +179,7 @@ function stepBoard(board) {
             var live = t + b + l + r + tl + tr + bl + br;
             // console.log(live);
 
-            if(board[i][j]) {
+            if(board.rows[i].cells[j].style.backgroundColor !== 'white') {
                 if(live === 2 || live === 3) {
                     newBoard[i][j] = true;
                 } else {
@@ -163,5 +195,14 @@ function stepBoard(board) {
         }
     }
 
-    return newBoard;
+    for(var i = 0; i < h; i++ ) {
+        for(var j = 0; j < w; j++ ) {
+            if(newBoard[i][j]){
+                board.rows[i].cells[j].style.backgroundColor = colorArr[Math.floor(Math.random() * 10)];
+            } else {
+                board.rows[i].cells[j].style.backgroundColor = 'white';
+            }
+        }
+    }
+    
 }
